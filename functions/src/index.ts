538adcb9ce57b0565b0947898a277627803158dd
Milos Kresovic <kresovic.milos@gmail.com>
2021-10-01 15:26:43 +0200

    Set up email from form

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,42 @@
 import * as functions from "firebase-functions";
+import admin from "firebase-admin";
+import * as nodemailer from "nodemailer";
+import cors from "cors";
+const corsHandler = cors({ origin: true });
+admin.initializeApp();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "testovski863@gmail.com",
+    pass: "863testovski!",
+  },
+});
+
+exports.sendMail = functions.https.onRequest((req, res) => {
+  corsHandler(req, res, async () => {
+    const { email, message, name, phone } = req.body;
+
+    const mailOptions = {
+      from: `${name} testovski863@gmail.com`,
+      to: "darko.dedovic86@gmail.com",
+      subject: "I'M A PICKLE!!!", // email subject
+      html: `<p style="font-size: 16px;">Pickle Riiiiiiiiiiiiiiiick!!</p>
+                <br />
+            
+            <p>Telefon: ${phone}</p>
+            <p>Email: ${email}</p>
+            <h3>Poruka:</h3>
+            <p>${message}</p>`,
+    };
+
+    // returning result
+    return transporter.sendMail(mailOptions, (erro, info) => {
+      if (erro) {
+        return res.send(erro.toString());
+      }
+      return res.send(info);
+    });
+  });
+});
